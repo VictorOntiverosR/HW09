7af5dfd0fb0f0ac105fe7a39a1f4497a36f1390c
VictorOntiverosR <156631200+VictorOntiverosR@users.noreply.github.com>
2024-03-31 03:14:43 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup( name= 'Homework09',
+      version= 01,
+      description= 'Homework Demostration Package',
+      url= 'https://github.com/VictorOntiverosR/HW09',
+      author= 'Victor Ontiveros',
+      author_email= 'qfw3cr@virginia.edu',
+      license= 'MIT',
+      packages=[ 'booklover', 'booklover_test']
+      )
